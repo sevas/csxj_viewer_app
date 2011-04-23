a746c47b2d29d2fd9d4a98728ca0f03cc6685f92
sevas <f.degroef@gmail.com>
2011-04-23 15:27:30 +0200

    cleanup of old stuff

--- urls.py
+++ urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
+    url(r'^provider/.*$', include('befr_news_viewer_app.urls')),
     url(r'^$', 'befr_news_viewer_app.summary.views.index'),
+
     # url(r'^befr_news_viewer_app/', include('befr_news_viewer_app.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -18,9 +18,3 @@ urlpatterns = patterns('',
     #url(r'^admin/', include(admin.site.urls)),
 )
 
-#urlpatterns += staticfiles_urlpatterns()
-
-#if settings.DEBUG:
-#    urlpatterns += patterns('',
-#        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-#    )
