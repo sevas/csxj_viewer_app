0e5a68c3bccdb21c14e72643a85c9e3828cc0f5a
sevas <f.degroef@gmail.com>
2011-04-25 03:04:24 +0200

    fixed the urls

--- provider/urls.py
+++ provider/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
                        url(r'^(.*)/(\d\d\d\d)-(\d\d)-(\d\d)/(\d\d\d\d)$', 'provider.views.show_source_day_batch_summary'),
                        url(r'^(.*)/(\d\d\d\d)-(\d\d)-(\d\d)$', 'provider.views.show_source_day_summary'),
-                       url(r'^(\p{Alpha}*)$', 'provider.views.show_source_summary'),
+                       url(r'^(.+)$', 'provider.views.show_source_summary'),
                        url(r'^$', 'provider.views.index'),)
 
 
