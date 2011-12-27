633180da9f8306fcc03908f7cd9e632e3bd63710
sevas <f.degroef@gmail.com>
2011-12-28 00:23:12 +0100

    updated apache conf file and wsgi script
    --HG--
    rename : apache/befr_news_viewer_app.apachevhostsample => apache/csxj_viewer_app.apachevhostsample
    rename : apache/django_tartiflette.wsgi => apache/django_csxj_tartiflette.wsgi

--- conf.py
+++ conf.py
@@ -3,7 +3,7 @@ __author__ = 'sevas'
 import os.path
 
 if LIVE:
-    JSON_DATABASE_PATH = '/home/www/django-sites/data/befr_news_viewer_database'
+    JSON_DATABASE_PATH = '/home/www/django-sites/data/csxj_viewer_database'
 else:
     JSON_DATABASE_PATH = os.path.join(os.path.dirname(__file__), 'static_data')
 
