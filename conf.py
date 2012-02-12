4eb3121e5a27da54b22149b1a345308ca16056a8
sevas <f.degroef@gmail.com>
2012-02-12 23:37:35 +0100

    bumped version id

--- conf.py
+++ conf.py
@@ -2,8 +2,9 @@ __author__ = 'sevas'
 
 import os.path
 
+LIVE=False
+
 if LIVE:
     JSON_DATABASE_PATH = '/home/www/django-sites/data/csxj_viewer_database'
 else:
     JSON_DATABASE_PATH = os.path.join(os.path.dirname(__file__), 'static_data')
-
